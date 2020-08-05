0e786308606d07346197ca846e305cbe4b740fb3
nayara-student <itgnosebrasil@gmail.com>
2020-08-05 20:47:02 -0300

    padrão facade

---
+++ ContaBancaria.java
@@ -0,0 +1,35 @@
+
+public class ContaBancaria {
+	private String conta;
+	private int saldo;
+
+	public int depositar(int valor){
+		System.out.println("Depositando...");
+		return this.setSaldo(this.getSaldo() + valor);
+	}
+
+	public String obterNumConta(){
+		System.out.println("Obtendo número de conta...");
+		return this.conta;
+	}
+
+	public int obterSaldo(){
+		System.out.println("Obtendo saldo...");
+		return this.getSaldo();
+	}
+
+	public ContaBancaria(){}
+
+	public ContaBancaria(String conta){
+		this.conta = conta;
+	}
+
+	public int getSaldo() {
+		return saldo;
+	}
+
+	public int setSaldo(int saldo) {
+		this.saldo = saldo;
+		return saldo;
+	}
+}
